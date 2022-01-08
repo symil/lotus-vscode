eef213d03c743d3f7915abc22e6f3120b200ce5b
Sylvain Milan <milan.sylvain@gmail.com>
2022-01-08 05:05:43 +0100

    Remove parameter hints on autocompletion.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -47,7 +47,10 @@ export function activate(context: ExtensionContext) {
 }
 
 async function provideSignatureHelp(document: TextDocument, position: Position, token: CancellationToken, context: SignatureHelpContext): Promise<SignatureHelp> {
+	// let start = Date.now();
 	let output = await languageServer.command('provide-signature-help', { document, position, sendContent: true });
+	// let end = Date.now();
+	// log(`client: ${end - start}ms`);
 	let result : SignatureHelp | null = null;
 
 	for (let { type, items } of output) {
@@ -92,12 +95,12 @@ async function provideCompletionItems(document: TextDocument, position: Position
 			completionItem.detail = detail;
 			completionItem.documentation = documentation;
 
-			if (completionItem.kind === CompletionItemKind.Function || completionItem.kind === CompletionItemKind.Method) {
-				completionItem.command = {
-					title: 'signature help',
-					command: 'editor.action.triggerParameterHints'
-				};
-			}
+			// if (completionItem.kind === CompletionItemKind.Function || completionItem.kind === CompletionItemKind.Method) {
+			// 	completionItem.command = {
+			// 		title: 'signature help',
+			// 		command: 'editor.action.triggerParameterHints'
+			// 	};
+			// }
 
 			if (insertText) {
 				completionItem.insertText = new SnippetString(insertText);
