88ad9468a302bccc12b64a562e56e91b98daccfc
Sylvain Milan <milan.sylvain@gmail.com>
2022-01-15 02:23:23 +0100

    Implement code actions provider.

--- client/src/features/code-actions.ts
+++ client/src/features/code-actions.ts
@@ -1,5 +1,6 @@
-import { CancellationToken, CodeAction, CodeActionContext, CodeActionKind, languages, Range, Selection, TextDocument } from 'vscode';
-import { FeatureParameters } from '../utils';
+import { CancellationToken, CodeAction, CodeActionContext, CodeActionKind, languages, Range, Selection, TextDocument, Uri, workspace, WorkspaceEdit } from 'vscode';
+import { LanguageServer } from '../language-server';
+import { FeatureParameters, makeRange } from '../utils';
 
 export function registerCodeActionsProvider(parameters: FeatureParameters) {
 	languages.registerCodeActionsProvider(parameters.selector, {
@@ -7,8 +8,28 @@ export function registerCodeActionsProvider(parameters: FeatureParameters) {
 	});
 }
 
-async function provideCodeActions(document: TextDocument, range: Range | Selection, context: CodeActionContext, token: CancellationToken): Promise<CodeAction[]> {
-	return null;
+async function provideCodeActions(document: TextDocument, selection: Selection, context: CodeActionContext, token: CancellationToken): Promise<CodeAction[]> {
+	let output = await LanguageServer.command('provide-code-actions', { document, position: selection.active, sendContent: true });
+	let currentCodeAction : CodeAction;
+	let result = [];
+
+	for (let { type, items } of output) {
+		if (type === 'action') {
+			let [title, kind] = items;
+
+			currentCodeAction = new CodeAction(title, stringToCodeActionKind(kind));
+			currentCodeAction.edit = new WorkspaceEdit();
+			result.push(currentCodeAction);
+		} else if (type === 'replace') {
+			let [filePath, start, end, replacement] = items;
+			let fileUri = Uri.file(filePath);
+			let editedDocument = await workspace.openTextDocument(fileUri)
+
+			currentCodeAction.edit.replace(fileUri, makeRange(editedDocument, start, end), replacement);
+		}
+	}
+
+	return result;
 }
 
 function stringToCodeActionKind(string: string): CodeActionKind {
