54a25e49c9506005868d56307e0e92faf2152f96
Sylvain Milan <milan.sylvain@gmail.com>
2024-10-22 18:18:19 +0200

    Cleanup + add README.

--- README.md
+++ README.md
@@ -1,65 +1,27 @@
-# vscode-lotus README
+VS code extension for the [lotus](https://github.com/symil/lotus) programming language.
 
-This is the README for your extension "vscode-lotus". After writing up a brief description, we recommend including the following sections.
+## How to install
 
-## Features
+0. Install the [lotus compiler](https://github.com/symil/lotus).
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+1. Clone this repository in the VS code extension directory. On Linux, this is `$HOME/.vscode/extensions`.
 
-For example if there is an image subfolder under your extension project workspace:
+```sh
+git clone git@github.com:symil/lotus-vscode.git ~/.vscode/extensions
+```
 
-\!\[feature X\]\(images/feature-x.png\)
+2. Compile the extension.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+```sh
+cd ~/.vscode/extensions/vscode-lotus
+npm install
+npm run compile
+```
 
-## Requirements
+3. Copy the lotus compiler in the `server` directory of the extension.
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+```sh
+cp /path/to/lotus/target/release/lotus-compiler ~/.vscode/extensions/vscode-lotus/server
+```
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+4. Reload VS code.
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,8 +1,7 @@
 import * as path from 'path';
-import { execSync } from 'child_process';
-import { workspace, languages, ExtensionContext, TextDocument, Position, Range, CancellationToken, ProviderResult, WorkspaceEdit, Diagnostic, DiagnosticSeverity, Uri, window, Definition, Location, Hover, MarkdownString, CompletionContext, CompletionItem, CompletionList, SnippetString, SignatureHelpContext, SignatureHelp, SignatureInformation, ParameterInformation, CompletionItemKind, TextEditor, TextEditorEdit, commands, TextEdit, Selection, CodeActionContext, Command, CodeAction } from 'vscode';
+import { workspace, languages, ExtensionContext, window } from 'vscode';
 import { LanguageServer, ServerParameters } from './language-server';
-import { FeatureParameters, getFormattedTime, makeRange } from './utils';
+import { FeatureParameters, getFormattedTime } from './utils';
 import { registerValidationProvider } from './features/validate';
 import { registerCompletionItemProvider } from './features/completion-item';
 import { registerCodeActionsProvider } from './features/code-actions';
@@ -11,10 +10,8 @@ import { registerHoverProvider } from './features/hover';
 import { registerRenameProvider } from './features/rename';
 import { registerSignatureHelpProvider } from './features/signature-help';
 
-const SERVER_BINARY_NAME = 'lotus-compiler';
 const EXTENSION_ROOT_PATH = path.join(__dirname, '..', '..');
 const SELF_SERVER_ROOT_PATH = path.join(EXTENSION_ROOT_PATH, 'server');
-const SYSTEM_SERVER_ROOT_PATH = path.join(process.env.HOME || '', 'prog', 'lotus', 'lotus-compiler');
 const LANGUAGE_ID = 'lotus';
 
 let outputChannel = window.createOutputChannel('Lotus');
@@ -65,43 +62,17 @@ function areDifferentServerParameters(oldParams: ServerParameters, newParams: Se
 
 function getServerParameters() {
 	let config = workspace.getConfiguration();
-	let serverVersion = config.get('lotus.languageServerVersion', 'self') as string;
-	let serverPath = getServerPath(serverVersion);
+	let serverPath = getServerPath();
 	let logRequestDuration = config.get('lotus.logRequestDuration', false) as boolean;
 
 	return {
-		serverVersion,
 		serverPath,
 		logRequestDuration
 	}
 }
 
-function getServerPath(version): string {
-	let rootPath = '';
-	let serverPath = '';
-
-	if (version === 'self') {
-		rootPath = SELF_SERVER_ROOT_PATH;
-	} else if (version.startsWith('system')) {
-		let releaseOption = '';
-		let targetDirectory = 'debug';
-
-		if (version.includes('release')) {
-			releaseOption = '--release';
-			targetDirectory = 'release';
-		}
-
-		// log(`compiling system language server in ${targetDirectory} mode...`);
-		// execSync(`cd ${SYSTEM_SERVER_ROOT_PATH} && cargo build ${releaseOption}`);
-
-		rootPath = path.join(SYSTEM_SERVER_ROOT_PATH, 'target', targetDirectory);
-	}
-
-	if (rootPath) {
-		serverPath = path.join(rootPath, SERVER_BINARY_NAME);
-	}
-
-	return serverPath;
+function getServerPath(): string {
+	return SELF_SERVER_ROOT_PATH;
 }
 
 function log(string: string) {
